80bc72f2fbaca0e4874b740b3d4bae4220db29c4
SukhetskyiS <medpredin@gmail.com>
2023-04-30 23:28:23 +0300

    f

--- src/components/Cases/Cases.jsx
+++ src/components/Cases/Cases.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
   CasesContainer,
   CasesHeader,
@@ -5,10 +6,41 @@ import {
   CasesText,
   List,
   Image,
+  Modal,
+  ModalContent,
+  ModalImage,
+  ModalArrowLeft,
+  ModalArrowRight,
+  ModalCloseButton,
 } from "./Cases.styled";
 import { images } from "./images";
 
 export function Cases() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] =
+    useState(0);
+
+  const handleImageClick = (index) => {
+    setCurrentImageIndex(index);
+    setModalOpen(true);
+  };
+
+  const handlePreviousClick = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNextClick = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
+
   return (
     <CasesContainer id="cases">
       <CasesQuestion>This is what we do</CasesQuestion>
@@ -25,10 +57,23 @@ export function Cases() {
               srcSet={`${image.srcSet}, ${image.webpSrcSet}`}
               sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
               alt={`Cases ${index + 1}`}
+              onClick={() => handleImageClick(index)}
             />
           </li>
         ))}
       </List>
+      {modalOpen && (
+        <Modal>
+          <ModalContent>
+            <ModalImage
+              src={images[currentImageIndex].src}
+            />
+            <ModalArrowLeft onClick={handlePreviousClick} />
+            <ModalArrowRight onClick={handleNextClick} />
+            <ModalCloseButton onClick={handleModalClose} />
+          </ModalContent>
+        </Modal>
+      )}
     </CasesContainer>
   );
 }
--- src/components/Cases/Cases.styled.jsx
+++ src/components/Cases/Cases.styled.jsx
@@ -39,3 +39,80 @@ export const List = styled.ul`
 export const Image = styled.img`
   min-width: 280px;
 `;
+
+export const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 100;
+`;
+
+export const ModalContent = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  width: 90%;
+  height: 50%;
+  background-color: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+`;
+
+export const ModalImage = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+`;
+
+export const ModalArrowLeft = styled.button`
+  position: absolute;
+  top: 50%;
+  left: 10px;
+  transform: translateY(-50%);
+  background: none;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  z-index: 1;
+  &:before {
+    content: "<";
+    font-size: 2rem;
+  }
+`;
+
+export const ModalArrowRight = styled.button`
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  background: none;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  z-index: 1;
+  &:before {
+    content: ">";
+    font-size: 2rem;
+  }
+`;
+
+export const ModalCloseButton = styled.button`
+  position: absolute;
+  bottom: -20px;
+  right: 10px;
+  background: none;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  z-index: 1;
+  &:before {
+    content: "X";
+    font-size: 2rem;
+  }
+`;
